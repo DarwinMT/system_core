26b58f1591d85a47ff8021d4dd47765495333eef
DarwinDeveloper <darwinpeacehope@gmail.com>
2017-10-05 16:48:14 -0500

    test

--- public/app/app/LSysMedico/Agenda/AgendaController.js
+++ public/app/app/LSysMedico/Agenda/AgendaController.js
@@ -7,25 +7,22 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
         ignoreReadonly: true
     });
 
+    $scope.fecha_inicial="2017-10-01";
+    $scope.tipo_calendar="M";
+    $scope.meses_letras=["ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"];
 
-    ///--- crear calendario 
+    ///--- crear calendario mensual
     $scope.mes=[];
-    $scope.calendar=function(){
-        var feha=new Date();
+    $scope.calendar=function(fecha_init){
+        var today=new Date();
+
+        $scope.mes=[];
+        var aux=fecha_init.split("-");
+        var feha=new Date(parseInt(aux[0]), (parseInt(aux[1])-1) , 1 );
 
         var fecha_inicial = new Date(feha.getFullYear(), feha.getMonth(), 1);
         var fecha_final = new Date(feha.getFullYear(), feha.getMonth() + 1, 0);
 
-        var numero_dias=((fecha_final-fecha_inicial)/(1000*60*60*24));
-
-        /*
-        console.log(fecha_inicial);
-        console.log(fecha_final);
-
-        console.log(fecha_inicial.getFullYear() +"-"+ fecha_inicial.getMonth()+"-"+ fecha_inicial.getDate());
-        console.log(fecha_final.getFullYear() +"-"+ fecha_final.getMonth()+"-"+ fecha_final.getDate());
-        console.log(numero_dias);
-        console.log(fecha_inicial.getDay());*/
 
         var aux_d=0;
         var primer_dia=1;
@@ -37,11 +34,16 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
         var semana3=[];
         var semana4=[];
         var semana5=[];
+        var semana6=[];
         for(var x=1; x<=fecha_final.getDate();x++){
 
-
+            var hoy_actual=0;
+            if(x==today.getDate()){
+                hoy_actual=1;
+            }
             if(aux_d<=6){
-                                
+                
+
                 var aux_fecha= new Date(fecha_final.getFullYear(), fecha_final.getMonth(), x);
 
                 switch(aux_semanas){
@@ -51,43 +53,60 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                                 var dia={
                                     Id:'',
                                     Numero_dia:'',
-                                    Numero_Citas:''
+                                    Numero_Citas:'',
+                                    Hoy:hoy_actual
                                 };
                                 semana1.push(dia);
+                                aux_d++;
                             }
+
                             var dia={
                                     Id:'',
                                     Numero_dia:x,
-                                    Numero_Citas:'8'
+                                    Numero_Citas:'8',
+                                    Hoy:hoy_actual
                                 };
                             semana1.push(dia);
+
                         }else{
                             if(semana1.length<=6){
                                 var dia={
                                     Id:'',
                                     Numero_dia:x,
-                                    Numero_Citas:'8'
+                                    Numero_Citas:'8',
+                                    Hoy:hoy_actual
                                 };
                                 semana1.push(dia);
+                                
+                            }else{
+
                             }
+
+
                         }
+
 
                     break;
 
                     case 2:
+
+                    
                             var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'12'
+                                Numero_Citas:'12',
+                                Hoy:hoy_actual
                             };
                             semana2.push(dia);
                     
                     break;
                     case 3:
+
                         var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'5'
+                                Numero_Citas:'5',
+                                Hoy:hoy_actual
                             };
                         semana3.push(dia);
                         
@@ -96,21 +115,36 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                         var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'741'
+                                Numero_Citas:'741',
+                                Hoy:hoy_actual
                             };
                         semana4.push(dia);                        
                     break;
                     case 5:
+
                         var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'798'
+                                Numero_Citas:'798',
+                                Hoy:hoy_actual
                             };
                         semana5.push(dia);                        
+                    break;
+                    case 6:
+
+                        var dia={
+                                Id:'',
+                                Numero_dia:x,
+                                Numero_Citas:'8555',
+                                Hoy:hoy_actual
+                            };
+                        semana6.push(dia);                        
                     break;
                 }
 
             }else{
+
+               
 
                 aux_semanas++;
                 switch(aux_semanas){
@@ -118,7 +152,8 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                             var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'13'
+                                Numero_Citas:'13',
+                                Hoy:hoy_actual
                             };
                             semana2.push(dia)
                     break;
@@ -126,7 +161,8 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                         var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'1'
+                                Numero_Citas:'1',
+                                Hoy:hoy_actual
                             };
                         semana3.push(dia);
                     break;
@@ -135,7 +171,8 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                         var dia={
                             Id:'',
                             Numero_dia:x,
-                            Numero_Citas:'1222'
+                            Numero_Citas:'1222',
+                            Hoy:hoy_actual
                         };
                         semana4.push(dia);
                     break;
@@ -144,9 +181,20 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
                         var dia={
                                 Id:'',
                                 Numero_dia:x,
-                                Numero_Citas:'-551'
+                                Numero_Citas:'-551',
+                                Hoy:hoy_actual
                             };
                         semana5.push(dia);
+                    break;
+                    case 6:
+
+                        var dia={
+                                Id:'',
+                                Numero_dia:x,
+                                Numero_Citas:'-55',
+                                Hoy:hoy_actual
+                            };
+                        semana6.push(dia);                        
                     break;
                 }
 
@@ -157,18 +205,70 @@ app.controller('LogicaAgenda', function($scope, $http, API_URL,Upload) {
             }
             aux_d++;
         }
-        console.log(semana1);
 
         $scope.mes.push(semana1);
         $scope.mes.push(semana2);
         $scope.mes.push(semana3);
         $scope.mes.push(semana4);
         $scope.mes.push(semana5);
-        console.log($scope.mes)
+        $scope.mes.push(semana6);
+    };
+    ///--- crear calendario mensual
 
+    ///--- retroceder y adelantar calendario
+    $scope.back_calendar=function(){
+
+        switch($scope.tipo_calendar){
+            case "M": // mensual
+                var aux_mes= $scope.fecha_inicial;
+                var aux = aux_mes.split("-");
+                var fecha_back= new Date(parseInt(aux[0]), (parseInt(aux[1]) -1 ),1);
+                var dayOfMonth = fecha_back.getMonth();
+                fecha_back.setMonth(dayOfMonth-1); //restar mes
+                var aux_mes_suma=(fecha_back.getMonth()+1);
+                var aux_mes = (aux_mes_suma.toString().length==1)? "0"+(aux_mes_suma):(aux_mes_suma);
+                var aux_dias = (fecha_back.getDate().toString().length==1)? "0"+fecha_back.getDate():fecha_back.getDate();
+                $scope.fecha_inicial=fecha_back.getFullYear()+"-"+aux_mes+"-"+aux_dias;
+                $scope.calendar($scope.fecha_inicial);
+            break;
+        };
+        $scope.titulo_fecha();
+    };
+
+    $scope.nex_calendar=function(){
+        switch($scope.tipo_calendar){
+            case "M": // mensual
+                var aux_mes= $scope.fecha_inicial;
+                var aux = aux_mes.split("-");
+                
+                var fecha_next= new Date(parseInt(aux[0]), (parseInt(aux[1])-1),1);
+                var dayOfMonth2 = fecha_next.getMonth();
+                fecha_next.setMonth(dayOfMonth2 + 1); //sumar mes
+
+                var aux_mes_suma=(fecha_next.getMonth()+1);
+                var aux_mes = (aux_mes_suma.toString().length==1)? "0"+(aux_mes_suma):(aux_mes_suma);
+                var aux_dias = (fecha_next.getDate().toString().length==1)? "0"+fecha_next.getDate():fecha_next.getDate();
+                $scope.fecha_inicial=fecha_next.getFullYear()+"-"+aux_mes+"-"+aux_dias;
+                $scope.calendar($scope.fecha_inicial);
+            break;
+        };
+        $scope.titulo_fecha();
+    };
+
+    $scope.Fecha_Select="";
+    $scope.titulo_fecha=function(){
+        var aux = $scope.fecha_inicial.split("-");
+        switch($scope.tipo_calendar){
+            case "M":
+                $scope.Fecha_Select=($scope.meses_letras[(parseInt(aux[1]-1))])+" Del "+aux[0];
+            break;
+        };
 
     };
-    $scope.calendar();
+    ///--- retroceder y adelantar calendario
+
+    $scope.calendar($scope.fecha_inicial);
+    $scope.titulo_fecha();
 
 
 });
--- public/app/view/bo/LSysMedico/Agenda/ViewAgenda.php
+++ public/app/view/bo/LSysMedico/Agenda/ViewAgenda.php
@@ -24,8 +24,14 @@
 			</div>
 			<div class="col-md-5 col-xs-12">
 				<div class="btn-group" >
-					<button type="button" class="btn btn-primary btn-sm"><i class="glyphicon glyphicon-backward "></i> Atras</button>
-					<button type="button" class="btn btn-primary btn-sm"><i class="glyphicon glyphicon-forward "></i> Adelante</button>
+					<button type="button" ng-click="back_calendar();" class="btn btn-primary btn-sm">
+							<i class="glyphicon glyphicon-backward "></i> Atras
+					</button>
+					
+					<button type="button" ng-click="nex_calendar();" class="btn btn-primary btn-sm">
+							<i class="glyphicon glyphicon-forward "></i> Adelante
+					</button>
+
 					<button type="button" class="btn btn-info btn-sm"><i class="glyphicon glyphicon-calendar "></i> Año</button>
 					<button type="button" class="btn btn-info btn-sm"><i class="glyphicon glyphicon-calendar "></i> Mes</button>
 					<button type="button" class="btn btn-info btn-sm"><i class="glyphicon glyphicon-calendar "></i> Semana</button>
@@ -67,24 +73,33 @@
 				<table class="table table-bordered table-condensend">
 					<thead>
 						<tr class="bg-primary">
-							<th>Domingo</th>
-							<th>Lunes</th>
-							<th>Martes</th>
-							<th>Miercoles</th>
-							<th>Jueves</th>
-							<th>Viernes</th>
-							<th>Sabado</th>
+							<th colspan="7" class="text-center">{{Fecha_Select}}</th>
+						</tr>
+						<tr class="bg-primary">
+							<th style="width: 100px;">Domingo</th>
+							<th style="width: 100px;">Lunes</th>
+							<th style="width: 100px;">Martes</th>
+							<th style="width: 100px;">Miercoles</th>
+							<th style="width: 100px;">Jueves</th>
+							<th style="width: 100px;">Viernes</th>
+							<th style="width: 100px;">Sabado</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr ng-repeat="m in mes">
-							<td ng-repeat="d in m">
+							<td ng-repeat="d in m" style="height: 100px;">
 								<div class="row">
 									<div class="col-xs-6">
 										<button onclick="$('#citas').modal('show');" class="btn btn-sm btn-success"><span class="badge" >{{d.Numero_Citas}}</span></button>
 									</div>
-									<div class="col-xs-6 text-right" >
+									<div class="col-xs-6 text-right " ng-hide="d.Hoy!=0 "  >
 										{{d.Numero_dia}}
+									</div>
+
+									<div class="col-xs-6 text-center " ng-hide="d.Hoy==0 "  >
+										<div style="width: 30px; height: 30px; background-color:#81d4fa; border-radius: 50%; float: right;">
+											{{d.Numero_dia}}
+										</div>
 									</div>
 								</div>
 							</td>
