7deef4f5caef91a993e64fb418e2dc529e85eecb
Darwin <darwinpeacehope@gmail.com>
2018-01-11 11:19:31 -0500

    test

--- resources/views/Home/Homem.php
+++ resources/views/Home/Homem.php
@@ -9,22 +9,43 @@
     <!--Jquery-->
     <script src="<?= asset('../js/jquery-3.1.1.min.js') ?>"></script>
 
+  
     <!--Materialize-->
     <!--Import Google Icon Font-->
       	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<script src="<?= asset('../assetsm/js/materialize.min.js') ?>"></script>
-	<link href="<?= asset('../assetsm/css/materialize.min.css') ?>" rel="stylesheet">
-	
+      <!--	<script src="<?= asset('../assetsm/js/materialize.min.js') ?>"></script>
+      	<link href="<?= asset('../assetsm/css/materialize.min.css') ?>" rel="stylesheet">-->
+       <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+      <!-- Compiled and minified JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
 	<!--Angular-->
 	<script src="<?= asset('../app/angular/angular.min.js') ?>"></script>
 	<script src="<?= asset('../app/angular/angular-route.min.js') ?>"></script>
-		<!--Aplicacion-->
-	<script src="<?= asset('../app/app/app.js') ?>"></script>
+		
+    <!--Upload angular-->
+      <script src="<?= asset('../js/ng-file-upload-shim.min.js') ?>"></script>
+      <script src="<?= asset('../js/ng-file-upload.min.js') ?>"></script>
+  <!--Aplicacion-->
+	<script src="<?= asset('../app/app/appm.js') ?>"></script>
 		<!--Controller-->
 	<script src="<?= asset('../app/app/login/Start.js') ?>"></script>		
 
+<style type="text/css">
+ /* body { 
+    background: url(https://i.pinimg.com/originals/bd/6c/f8/bd6cf8fd2c1f11c84d455051518923d4.jpg) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }*/
+</style>
+
 </head>
-<body ng-controller="Login">
+<body ng-controller="Login" >
+
+
 
   <!-- Start Page Loading -->
   <div id="loader-wrapper">
@@ -35,7 +56,8 @@
   <!-- End Page Loading -->
 
 
- <div id="login-page" class="row">
+ <div id="login-page" class="row" style="padding: 10%;">
+    
     <div class="col s12 z-depth-4 card-panel">
       <form class="login-form" name="logi_system" id="logi_system"  novalidate="">
         <div class="row">
@@ -63,7 +85,7 @@
        	
        	<div class="row margin" ng-hide="Mensaje_Start=='' " ng-show=" Mensaje_Start!='' ">
        		<div class="col s12">
-       			<div class="card yellow darken-1">
+       			<div class="card red lighten-4">
 		            <div class="card-content black-text waves-light center-align ">
 		            	{{Mensaje_Start}}
 		            </div>
@@ -74,14 +96,15 @@
 
         <div class="row">
           <div class="input-field col s12">
-            <button type="button" ng-click="start_login();" ng-disabled="logi_system.$invalid" class="btn waves-effect waves-light col s12 indigo" > Login </button>
+            <button type="button" ng-click="start_login();" ng-disabled="logi_system.$invalid" class="btn waves-effect pulse  waves-light col s12 " > Login </button>
           </div>
         </div>
-        
 
       </form>
     </div>
   </div>
+
+
 
 </body>
 </html>
