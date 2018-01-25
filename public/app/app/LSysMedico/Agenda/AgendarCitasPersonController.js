400a7b1a9f0498215f1b5f33df5819959e7394f2
Darwin <darwinpeacehope@gmail.com>
2018-01-24 23:11:16 -0500

    test

--- public/app/app/LSysMedico/Agenda/AgendarCitasPersonController.js
+++ public/app/app/LSysMedico/Agenda/AgendarCitasPersonController.js
@@ -13,6 +13,9 @@ app.controller('LogicaAgendaCitasPersona', function($scope, $http, API_URL,Uploa
     $scope.observacion="";
     $scope.hora="";
 
+    $scope.nombrecliente="";
+    $scope.aux_cliente=null; 
+
     ///---
     $scope.list_permisos=[];
     $scope.permisos_user=function () {
@@ -30,6 +33,69 @@ app.controller('LogicaAgendaCitasPersona', function($scope, $http, API_URL,Uploa
     ///---
 
 
+///--- datos de usuario para la agenda
+    $scope.user_agenda={};
+    $scope.aux_empleado={};
+    $scope.get_user_agenda=function () {
+        $http.get(API_URL + 'Agenda/get_user_agenda')
+            .success(function(response){
+                console.log(response);
+                $scope.user_agenda=response[0];
+                $scope.select_empleado($scope.user_agenda);
+                $scope.Title="Agenda De Citas De "+$scope.user_agenda.persona.apellido+" "+$scope.user_agenda.persona.nombre;
+            });
+    };
+    $scope.get_user_agenda();
+     $scope.select_empleado=function(item){
+        $scope.aux_empleado=null;
+        $scope.aux_empleado=item;
+        console.log($scope.aux_empleado)
+    };
+///--- datos de usuario para la agenda
+
+    ///--- modal cliente
+    $scope.buscar_cliente=function(){
+        $("#md_cliente").modal("open");
+        $scope.pageChanged_cliente(1);
+    };
+    ///--- modal cliente
+
+    ///--- lista de cliente 
+    $scope.buscartexto_cliente="";
+
+    $scope.list_cliente=[];
+    $scope.pageChanged_cliente = function(newPage) {
+        $scope.initLoad_cliente(newPage);
+    };
+    $scope.initLoad_cliente = function(pageNumber){
+
+        var filtros = {
+            buscar:$scope.buscartexto_cliente,
+            estado: "1"
+        };
+        $http.get(API_URL + 'Cliente/get_list_cliente?page=' + pageNumber + '&filter=' + JSON.stringify(filtros))
+            .then(function(response){
+                $scope.list_cliente = response.data.data;
+                $scope.totalItems = response.data.total;
+                console.log($scope.list_cliente);
+         });
+    };
+    ///--- lista de cliente 
+    ///--- seleccione cliente
+    $scope.select_cliente=function(item){
+        $scope.nombrecliente="";
+        $scope.aux_cliente=null; 
+        $scope.aux_cliente=item;
+        $scope.nombrecliente=item.persona.apellido+" "+item.persona.nombre;
+        $("#md_cliente").modal("close");
+    };    
+    ///--- seleccione cliente
+    //quitar cliente seleccionado
+    $scope.limpiar_cliente=function() {
+        $scope.nombrecliente="";
+        $scope.aux_cliente=null;
+    };
+    //quitar cliente seleccionado
 });
 
 function sms(color,mensaje) {
--- public/app/view/ma/LSysMedico/Agenda/ViewCrearCitas.php
+++ public/app/view/ma/LSysMedico/Agenda/ViewCrearCitas.php
@@ -1,14 +1,63 @@
 <div class="row" ng-cloak>
 
- <div id="modal1" class="modal">
+ <div id="md_cliente" class="modal">
     <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
+      <h5><strong>Buscar Cliente</strong></h5>
+      <hr>
+
+      <div class="row">
+      		<div class="col m10 s10 ">
+      			<div class="input-group">
+			      <input type="text" class="form-control input-sm"  placeholder="Buscar " name="buscartexto_cliente" ng-model="buscartexto_cliente" id="buscartexto_cliente">
+			    </div>
+      		</div>
+      		<div class="col m2 s2">
+      			<a class="btn-floating  waves-effect waves-light  blue " ng-click="pageChanged_cliente(1)" ><i class="material-icons">search</i></a>
+      		</div>
+      	</div>
+
+      	<div class="row">
+      		<div class="col m12 s12">
+      			<table class="table highlight bordered raaesponsive-table">
+      				<thead>
+      					<tr > 
+      						<th>DNI</th>
+      						<th>Cliente</th>
+      						
+      						<th></th>
+      					</tr>
+      				</thead>
+      				<tbody >
+						<tr dir-paginate="u in list_cliente | orderBy:sortKey:reverse |filter:buscartexto_cliente| itemsPerPage:5" total-items="totalItems" ng-cloak">
+							<td>{{u.persona.ci}}</td>
+							<td>{{u.persona.apellido+" "+u.persona.nombre}}</td>
+							<td>
+								<div class="btn-group" role="group" >
+									<button  ng-click="select_cliente(u);" type="button" title="Editar" class="waves-effect waves-light btn">
+										<i class="material-icons">send</i>
+									</button>
+								</div>
+							</td>
+						</tr>
+      				</tbody>
+      			</table>
+      			<dir-pagination-controls
+		        	on-page-change="pageChanged_cliente(newPageNumber)"
+		            template-url="dirPagination.html"
+		            class="pull-right"
+		            max-size="10"
+		            direction-links="true"
+		            boundary-links="true" >
+		        </dir-pagination-controls>
+      		</div>
+      	</div>
+
     </div>
     <div class="modal-footer">
-      <a href="" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
     </div>
-  </div>
+</div>
+
+
 
 	<div class="row">
 		<div class="col m12 s12">
@@ -28,8 +77,8 @@
 		</div>
 
 		<div class="col m12 s12 input-field">
-			<i class="material-icons prefix">face</i>
-          	<input type="text" class=""  name="nombrecliente" id="nombrecliente" ng-model="nombrecliente" required />
+			<i class="material-icons prefix" ng-click="buscar_cliente();">face</i>
+          	<input type="text" class="" disabled  name="nombrecliente" id="nombrecliente" ng-model="nombrecliente" required />
           	<span class="help-block error" ng-show="frm_agenda.nombrecliente.$invalid && frm_agenda.nombrecliente.$touched">El Cliente es requerido</span>
 		</div>
 
@@ -39,7 +88,7 @@
           	<span class="help-block error" ng-show="frm_agenda.fechacita.$invalid && frm_agenda.fechacita.$touched">La fecha es requerida</span>
 		</div>
 		
-		<div class="col m12 s12 input-field">
+		<div class="col m12 s12">
 			<select class="browser-default"  name="hora" id="hora" ng-model="hora" required>
 				<option value="">Seleccione</option>
 				<option ng-repeat=" t in horas_general" value="{{t}}">{{t}}</option>
@@ -66,9 +115,10 @@
 	      		<i class="material-icons">save</i>
 	      	</a>
 	      </li>
+	      
 	      <li>
-	      	<a class="btn-floating blue tooltipped" title="Buscar Cliente" data-position="left" data-delay="50" data-tooltip="Buscar Cliente" >
-	      		<i class="material-icons">search</i>
+	      	<a class="btn-floating red tooltipped" ng-click="limpiar_cliente()" title="Quitar Cliente" data-position="left" data-delay="50" data-tooltip="Quitar Cliente" >
+	      		<i class="material-icons">delete</i>
 	      	</a>
 	      </li>
 	    </ul>
